270891921e09a319fdcd524e9d94df3868346803
Vivek <audit536@gmail.com>
2020-07-29 12:08:15 +0530

    Add files via upload

--- tts.js
+++ tts.js
@@ -82,30 +82,14 @@ nextButton.addEventListener('click',function(){
   next=true;
 });
 
-let deferredPrompt;
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI to notify the user they can add to home screen
-  // a2hsButton.style.display = 'block';
+window.addEventListener("beforeinstallprompt", ev => { 
+  // Stop Chrome from asking _now_
+  ev.preventDefault();
 
-  a2hsButton.addEventListener('click', (e) => {
-    // hide our user interface that shows our A2HS button
-    // a2hsButton.style.display = 'none';
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
-  });
+  // Create your custom "add to home screen" button here if needed.
+  // Keep in mind that this event may be called multiple times, 
+  // so avoid creating multiple buttons!
+  a2hsButton.onclick = () => ev.prompt();
 });
 
 
