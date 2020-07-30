a857c3afc8b4d883244b93950c046922ed24d7cf
Vivek <audit536@gmail.com>
2020-07-31 00:05:52 +0530

    Add files via upload

--- tts.js
+++ tts.js
@@ -232,14 +232,14 @@ async function parseSentences(){
 //Waits until speaking is over
 //Returns promise after completion
 async function showReadingText(textPart){
-  // let screenLock = new NoSleep();
-  // screenLock.enable();
-  // console.log('Screen Locked!');
+  let screenLock = new NoSleep();
+  screenLock.enable();
+  console.log('Screen Locked!');
   readingText.textContent=textPart;
   inputVoice.scrollIntoView();
   await speaker(textPart);
-  // screenLock.disable();
-  // console.log('Screen Unlocked.');
+  screenLock.disable();
+  console.log('Screen Unlocked.');
   return new Promise(resolve => {resolve();});
 }
 
